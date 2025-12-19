3d1d652066ca45978876c3a0c3a28ee07237a24c
Ian <iwmccurdie123@gmail.com>
2025-12-19 13:40:30 -0600

    Fixed UI to display database information

--- public/index.js
+++ public/index.js
@@ -1,25 +1,55 @@
-const getMenus=async()=>{
-    const response=await fetch('/api/v1/menu')
+const getMenu = async () => {
+    const response = await fetch('/api/v1/menu')
+    return await response.json()
+}
+const getEvents = async () => {
+    const response = await fetch('/api/v1/events')
     return await response.json()
 }
 
 
-const loadMenus=async()=>{
-    const menus=await getMenus();
-    console.log(menus)
-    const container=document.getElementById('container')
-    container.innerHTML=""
-    menus.forEach(item => {
-        container.innerHTML+=
-    ` <div class="menu-card">
-    <img src="${item.url}"  alt="Cheeseburger">
-  <h3>${item.name}</h3>
-  <p>${item.description}</p>
-  <span>$${item.price}</span>
-  </div>
-    `
-});
+const loadMenu = async () => {
+    const menu = await getMenu();
+    //console.log(menu)
+    const container = document.getElementById('menu-container')
+    container.innerHTML = ""
+    menu.forEach(item => {
+        const menuItem = document.createElement("div")
+        menuItem.className = "menu-item"
+        menuItem.innerHTML =
+            `<div class="menu-card">
+            <img src="${item.url}"  alt="${item.name}">
+            <h3>${item.name}</h3>
+            <p>${item.description}</p>
+            <span>$${item.price}</span>
+            </div>`
 
-   
+        container.appendChild(menuItem)     
+    });
 }
-loadMenus()
+
+const loadEvents = async () => {
+    const events = await getEvents();
+    //console.log(menu)
+    const eventContainer = document.getElementById('event-container')
+    eventContainer.innerHTML = ""
+    events.forEach(event => {
+        const eventItem = document.createElement("div")
+        eventItem.className = "event-item"
+        eventItem.innerHTML =
+            `<div class="menu-card">
+            <h3>${event.name}</h3>
+            <p>${event.location}</p>
+            <span>${event.date}</span>
+            <span>${event.time}</span>
+            </div>`
+
+        eventContainer.appendChild(eventItem)
+    });
+}
+
+
+
+
+loadMenu()
+loadEvents()
